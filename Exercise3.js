5d90e5f9fb25d2cc68e4aedd03add55a1f1605a6
TheAdamJohnKiernan42 <112864916+TheAdamJohnKiernan42@users.noreply.github.com>
2022-09-30 11:21:02 +0100

    Add files via upload

---
+++ Exercise3.js
@@ -0,0 +1,51 @@
+/* Question 1
+*, -, / and + are operators
+*/
+
+/* Question 2
+'spam' is a variable and spam is a string
+*/
+
+/* Question 3
+string, number, boolean.
+*/
+
+/* Question 4
+21
+*/
+
+/* Question 5
+spamspamspam
+*/
+
+/* Question 6
+let number = Number(prompt("Enter a Number:"))
+if(number == 1){
+    console.log("Hello")
+}
+else if(number == 2){
+    console.log("Howdy")
+}
+else{
+    console.log("Greetings")
+}
+*/
+
+/*
+
+/* Question 7
+1. console is case sensitive, should be spelled console instead of Console.
+2. Same as above, CONSOLE should be spelled console.
+3. Same as above, Log should be spelled log.
+4. Missing . between console and log, should be console.log
+*/
+
+/* Question 8
+1. Syntax Error, console is spelt with a capital instead of without.
+2. Syntax Error, missing quotations.
+3. Syntax Error, missing bracket before "Hello world"
+4. Missing semicolon, should work fine though.
+5. Code will not work, code is displayed as note.
+6. My first Javascript program is not preface with "//" so code will not work as it cannot understand it.
+7. Should work perfectly fine.
+8. Will be displayed as a note and code won't work.
