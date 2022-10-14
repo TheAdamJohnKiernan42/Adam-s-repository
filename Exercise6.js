49389888ad7fb5cf15c89b617e202ebf021bea47
TheAdamJohnKiernan42 <112864916+TheAdamJohnKiernan42@users.noreply.github.com>
2022-10-14 11:56:24 +0100

    Add files via upload

--- Exercise6.js
+++ Exercise6.js
@@ -25,3 +25,41 @@ console.log(value)
 
 //Question 4
 let row = Number(prompt("How many rows do you want?"))
+let dots = 0;
+
+let star = "*";
+
+let star2 = star;
+
+do{
+
+    dots++;
+
+    console.log(star);
+
+    star = star + star2;
+
+   
+
+}while(dots != row);
+
+// Question 5
+let digit = Number(prompt("Enter in some numbers"))
+let digit2 = 0
+
+while(digit != 0){
+    digit = Math.floor(digit / 10);
+    digit2++; 
+}
+console.log(digit)
+
+// Question 6
+let userInput = Number(prompt("Please input a number for which the digits will be added"));
+let dig = 0;
+let Input = userInput;
+while(userInput > 0){
+    Input = userInput % 10;
+    userInput = Math.floor(userInput / 10);
+    dig = dig + Input;
+}
+console.log(dig);
